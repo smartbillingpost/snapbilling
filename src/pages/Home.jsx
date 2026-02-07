51c25c4bd206ed4ec5a88fdc66adbcaf1a97359a
Smartbilling <smartbillingpost@gmail.com>
2026-02-07 13:45:05 +0530

    Add SEO meta tags to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Hero from "../components/Hero";
 import Pillars from "../components/Pillars";
 import TrustHighlights from "../components/TrustHighlights";
@@ -5,6 +6,22 @@ import TrustHighlights from "../components/TrustHighlights";
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>
+          SmartBilling – Simple Billing and GST App for Small Businesses
+        </title>
+
+        <meta
+          name="description"
+          content="SmartBilling is a simple billing and GST app for retail shops, distributors, and small businesses in India. Manage billing, expenses, GST, and business reports easily."
+        />
+
+        <meta
+          name="keywords"
+          content="billing and gst app, gst billing app, billing app for small business, offline billing app, billing software india"
+        />
+      </Helmet>
+
       <Hero />
       <Pillars />
       <TrustHighlights />
